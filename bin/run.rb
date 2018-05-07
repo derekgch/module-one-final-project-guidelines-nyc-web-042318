d431c9556f8ac84437d5ea28b30fc0be2e77c5c7
Derek <derek@ttds-MacBook-Air.local>
2018-05-07 11:25:20 -0400

    add some files

--- bin/run.rb
+++ bin/run.rb
@@ -1,2 +1,8 @@
 require_relative 'config/environment'
 
+require_relative "../lib/fetch.rb"
+require_relative "../lib/command_line_interface.rb"
+
+welcome
+character = get_character_from_user
+show_character_movies(character)
