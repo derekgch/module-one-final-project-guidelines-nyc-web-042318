c8c40f9c2a6ec3235d8b8a33d75a23a616cd39a5
Derek <derek@ttds-MacBook-Air.local>
2018-05-07 11:48:18 -0400

    have the test table running

--- bin/run.rb
+++ bin/run.rb
@@ -1,8 +1,10 @@
-require_relative 'config/environment'
+require_relative '../config/environment.rb'
 
 require_relative "../lib/fetch.rb"
 require_relative "../lib/command_line_interface.rb"
 
+pry.start
+
 welcome
-character = get_character_from_user
-show_character_movies(character)
+# character = get_character_from_user
+# show_character_movies(character)
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 require 'bundler'
+require_relative "../lib/fetch.rb"
+require_relative "../lib/command_line_interface.rb"
 Bundler.require
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
 require_all 'lib'
+require_all 'app'
